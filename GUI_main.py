b90fa54c5e2fb5bc101303af21d368a44083f19b
jjeongvely <khj961127@naver.com>
2019-10-15 13:41:10 +0900

    얼굴등록 및 얼굴 인식

--- GUI_main.py
+++ GUI_main.py
@@ -1,7 +1,6 @@
 from multiprocessing import Process, Queue
 
 import sys
-from imutils import face_utils
 import datetime
 import face_recognition
 
@@ -20,54 +19,6 @@ from knn_face_recognition import *
 
 form_class = uic.loadUiType("GUI.ui")[0]
 
-CNN_INPUT_SIZE = 128
-
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
-(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
-
-# Alarm
-ALARM_ON_EYE = False
-ALARM_ON_MOUTH = False
-ALARM_ON_yaw = False
-ALARM_ON_pitch = False
-ALARM_ON_face = False
-
-SOUND_ALARM = False
-
-max_mouthLR = 0
-
-# buffer for judgement
-buf_YAW = []
-buf_PITCH = []
-buf_MAR = []
-buf_EAR = []
-buf_EAR_judge = []
-buf_MAR_judge = []
-buf_YAW_judge = []
-buf_PITCH_judge = []
-
-prev_time = 0
-
-# variables for judgement
-EAR = 0
-MAR = 0
-yaw = 0
-pitch = 0
-EAR_calib = 0
-MAR_calib = 0
-YAW_calib = 0
-PITCH_calib = 0
-
-# Icon for alarm
-ICON_RED_LED = "./icons/led_circle_red.svg.med.png"
-ICON_GREEN_LED = "./icons/led_circle_green.svg.med.png"
-ICON_GREY_LED = "./icons/led_circle_grey.svg.med.png"
-
-no_face_count = 0
-
-
 class MainWindow(QWidget, form_class):
     # class constructor
     def __init__(self):
@@ -78,8 +29,8 @@ class MainWindow(QWidget, form_class):
         self.edit.setText("Input name")
 
         # create a timer
-        self.view_timer = QTimer() # 화면 출력을 위한 timer
         # set timer timeout callback function
+        self.view_timer = QTimer() # viewCam함수 즉, 화면 출력을 위한 timer
         self.view_timer.timeout.connect(self.viewCam)
 
         self.detect_timer = QTimer()
@@ -96,10 +47,7 @@ class MainWindow(QWidget, form_class):
 
         # set detection callback clicked  function
         self.registration.clicked.connect(self.faceRegistration)
-        self.detection.clicked.connect(self.faceDetection)
-
-        # Introduce mark_detector to detect landmarks.
-        # self.mark_detector = MarkDetector()
+        self.detection.clicked.connect(self.faceDetection) # detection 버튼을 누르면 faceDetection 함수 호출
 
         # Setup process and queues for multiprocessing.
         self.save_queue = Queue()
@@ -126,22 +74,20 @@ class MainWindow(QWidget, form_class):
 
 
     def faceDetection(self):
+        # detect_timer가 stop상태이면 start시켜 detection 함수 호출
         if not self.detect_timer.isActive():
             self.log_browser.setText("[INFO] Start face detection")
-            # print("detection")
             self.detect_timer.start(0.1)
-            self.detection.setText("Stop Detection")
+            self.detection.setText("Stop")
+        # detection 버튼을 다시 누르면 detect_timer가 start상태이므로 stop으로 변경
         else:
             self.detect_timer.stop()
-            # print("no d")
             self.log_browser.setText("[INFO] Stop face detection")
             self.detection.setText("Start Detection")
 
 
     def Detection(self):
         # Grab a single frame of video
-        # if not self.detect_timer.isActive():
-        # print("start detection")
         ret, frame = self.cap.read()
         frame = cv2.flip(frame, 2)
 
@@ -163,6 +109,7 @@ class MainWindow(QWidget, form_class):
         # Note: You can pass in either a classifier file name or a classifier model instance
         predictions = predict(rgb_small_frame, model_path="trained_knn_model.clf")
 
+        # 화면의 사람을 예측하여 얼굴에 bounding box로 표시
         # Display results overlaid on an image
         for name, (top, right, bottom, left) in predictions:
             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
@@ -180,13 +127,14 @@ class MainWindow(QWidget, form_class):
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
             # print(name)
 
+        # cv2에서는 BGR이므로 RGB로 바꿈
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # get image infos
         height, width, channel = frame.shape
         step = channel * width
         # create QImage from image
-        qImg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
+        qImg = QImage(frame, width, height, step, QImage.Format_RGB888)
 
         # show image in img_label
         self.image_label.setPixmap(QPixmap.fromImage(qImg))
@@ -200,7 +148,6 @@ class MainWindow(QWidget, form_class):
         if self.save_bt.isDown() is True:
             name = self.edit.text()
             self.save_timer.stop()
-            # print(name)
             for i in range(10):
                 ret, frame = self.cap.read()
                 frame = cv2.flip(frame, 2)
@@ -242,7 +189,7 @@ class MainWindow(QWidget, form_class):
 
     # view camera
     def viewCam(self):
-        ret, frame = self.cap.read()
+        ret, frame = self.cap.read() # 화면을 읽어오기 - 실패시 ret가 false
 
         if ret is False:
             self.view_timer.stop()
@@ -252,6 +199,7 @@ class MainWindow(QWidget, form_class):
         # If frame comes from webcam, flip it so it looks like a mirror.
         frame = cv2.flip(frame, 2) # 화면 반전
 
+        # OpenCV에서는 BGR 순서로 저장하니까 BGR을 RGB로
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # get image infos
@@ -261,6 +209,7 @@ class MainWindow(QWidget, form_class):
         qImg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
 
         # show image in img_label
+        # QLabel은 이미지를 표시하는 라벨 위젯
         self.image_label.setPixmap(QPixmap.fromImage(qImg))
 
     # start/stop timer
